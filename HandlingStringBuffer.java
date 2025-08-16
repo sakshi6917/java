cd8fd9b28c669c943bee335a2e56de9509d47339
sakshi <sakshi6917@gmail.com>
2025-08-17 02:37:08 +0530

    Handling string

--- HandlingStringBuffer.java
+++ HandlingStringBuffer.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 class HandlingStringBuffer{
     pubkic static void main(String args[])
     {
-        
+        System.out.println("Hello there")
+        System.out.println("Hello sakshi")
     }
 }
