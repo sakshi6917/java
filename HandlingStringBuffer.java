125b1fb7725ff2809920750338745ea5d318a1d5
sakshi <sakshi6917@gmail.com>
2025-08-17 02:43:43 +0530

    hi

--- HandlingStringBuffer.java
+++ HandlingStringBuffer.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 class HandlingStringBuffer{
-    pubkic static void main(String args[])
+    public static void main(String args[])
     {
         System.out.println("Hello there")
         System.out.println("Hello sakshi")
