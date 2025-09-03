32f55d64ca1cf550104731711f52ae8f7ad124e9
sakshi <sakshi6917@gmail.com>
2025-09-03 11:41:47 +0530

    first to fourth

--- ClassConditionalStatement/third.java
+++ ClassConditionalStatement/third.java
@@ -1,4 +1,15 @@
 import java.util.Scanner;
 class third{
     public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int num=sc.nextInt();
+        if(num>0)
+          System.out.println(num +" is positive");
+        else if(num<0)
+          System.out.println(num +" is negative");
+        else
+          System.out.println(num +" is 0");
+    }
 }
