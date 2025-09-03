61ff0e54cceb6338ccb448c5c7570c56fbf8c803
sakshi <sakshi6917@gmail.com>
2025-09-03 11:58:56 +0530

    fourth

--- ClassConditionalStatement/first.java
+++ ClassConditionalStatement/first.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class first{
+class First{
     public static void main(String args[])
     {
         System.out.println("Enter the two numbers");
--- ClassConditionalStatement/fourth.java
+++ ClassConditionalStatement/fourth.java
@@ -1,2 +1,13 @@
 import java.util.Scanner;
-class fourth
+class Fourth{
+    public static void main(String args[])
+    {
+        System.out.println("Enter the number:");
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        if((num%5==0 )&& (num%11==0))
+          System.out.println(num +" is divisible by both 5 and 11");
+        else
+          System.out.println(num +" is not divisible by 5 and 11");
+    }
+}
