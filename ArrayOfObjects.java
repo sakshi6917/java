ac2840408b472ac89451a82de51b074f04e31161
sakshi <sakshi6917@gmail.com>
2025-09-05 20:20:21 +0530

    practice Array of Objects

--- ArrayOfObjects.java
+++ ArrayOfObjects.java
@@ -1,41 +1,39 @@
 import java.util.Scanner;
 class Student{
-    int roll;
     String name;
+    int roll;
     int marks;
-
 }
 class ArrayOfObjects{
     public static void main(String args[])
     {
         Student s1=new Student();
-        s1.roll=48;
-        s1.name="Shipra";
-        s1.marks=79;
+        s1.name="Meghna";
+        s1.roll=1;
+        s1.marks=89;
 
         Student s2=new Student();
-        s2.roll=21;
-        s2.name="Maahi";
-        s2.marks=82;
+        s2.name="Shilpi";
+        s2.roll=22;
+        s2.marks=76;
 
         Student s3=new Student();
-        s3.roll=35;
-        s3.name="Divya";
-        s3.marks=94;
-
-        Student students[]=new Student[3];
-        students[0]=s1;
-        students[1]=s2;
-        students[2]=s3;
-        for(int i=0;i<students.length;i++)
-        {
-            System.out.println(students[i].name + " : " + students[i].marks);
-        }
-
-        System.out.println();
-        for(Student stud:students)
-        {
-            System.out.println(stud.name + " : " + stud.marks);
-        }
+        s3.name="Kripa";
+        s3.roll=43;
+        s3.marks=97;
+       
+       Student students[]=new Student[3];             //not creating 3 ojects but creating an array which will hold student references 
+       students[0]=s1;
+       students[1]=s2;
+       students[2]=s3;
+       for(int i=0;i<students.length;i++)
+       {
+        System.out.println(students[i].name+ " : " + students[i].marks);
+       }
+       for(Student n:students)
+       {
+        System.out.println(n.name);
+       }
     }
 }
+
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+// class Student{
+//     int roll;
+//     String name;
+//     int marks;
+
+// }
+// class ArrayOfObjects{
+//     public static void main(String args[])
+//     {
+//         Student s1=new Student();
+//         s1.roll=48;
+//         s1.name="Shipra";
+//         s1.marks=79;
+
+//         Student s2=new Student();
+//         s2.roll=21;
+//         s2.name="Maahi";
+//         s2.marks=82;
+
+//         Student s3=new Student();
+//         s3.roll=35;
+//         s3.name="Divya";
+//         s3.marks=94;
+
+//         Student students[]=new Student[3];
+//         students[0]=s1;
+//         students[1]=s2;
+//         students[2]=s3;
+//         for(int i=0;i<students.length;i++)
+//         {
+//             System.out.println(students[i].name + " : " + students[i].marks);
+//         }
+
+//         System.out.println();
+//         for(Student stud:students)
+//         {
+//             System.out.println(stud.name + " : " + stud.marks);
+//         }
+//     }
+// }
